1be43d8c4027781e6fa3ef5e4b3711e0cc010514
Elton Longaray <elton.longaray483@gmail.com>
2026-07-19 11:50:22 -0300

    v289: remove as concordancias que ficaram soltas e a sombra retangular que marcava um quadrado em volta da corcova

--- js/components/menu-inferior.js
+++ js/components/menu-inferior.js
@@ -54,8 +54,6 @@ export function bottomNav(active) {
       <div class="belt-strap">
         <div class="belt-track">${itens}</div>
       </div>
-      <span class="belt-fillet f-esq" aria-hidden="true"></span>
-      <span class="belt-fillet f-dir" aria-hidden="true"></span>
       <div class="belt-bump" aria-hidden="true"></div>
       <div class="belt-lens" aria-hidden="true"></div>
     </nav>
